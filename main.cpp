0f505fe09fc3654441376e843f0425144a0ff92e
giosar-08 <giorgio.saracino@marconibari.edu.it>
2025-12-03 16:24:16 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int main ()
+float a,b,add,sott,mol,div,pot
+int i
+i=0
+while (i=1)
+   {
+    if (a>0 and b>0)
+    cout<< "numeri accettabili"
+    {
+       if a>0
+       cout<<"b non accettabile"
+       {
+        cout<<"a non accettabile"
+ add(int a, int b)
+   {
+    add=a+b
+   }
+ sott(int a, int b)
+   {
+    sott=a-b
+   }
+mol(int a, int b)
+    mol=a*b
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+div(int a, int b)
+   {
+    div=a/b
+   }
+   pot(int a, int b)
+   {
+    pot=a^b
+   }
+cout<<" i valori sono" & add,sott,mol,div,pot;
